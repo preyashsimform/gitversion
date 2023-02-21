9fcb47cd34b986fbeb3b12220648e8c50907082c
preyashsimform <preyash@simformsolutions.com>
2023-02-21 15:37:18 +0530

    commit after reset

--- trainee_frontend/src/App.js
+++ trainee_frontend/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, {useState} from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  axios.get("http://localhost:3002/",(req,res)=>setText(res.data))
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {text}
       </header>
     </div>
   );
