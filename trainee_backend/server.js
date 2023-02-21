6a71db75e3dcb05c71908a9a1a82cb2df6909582
preyashsimform <preyash@simformsolutions.com>
2023-02-21 15:11:15 +0530

    connect node with react

--- trainee_backend/server.js
+++ trainee_backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+    console.log("react app");
 });
 
-const PORT = 3001;
+const PORT = 3002;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-q
